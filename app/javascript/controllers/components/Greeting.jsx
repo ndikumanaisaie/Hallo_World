d7db00a4a17dff8b23f8a0c9ecf14c893311bcb5
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-28 23:00:51 +0200

    Add Greeting component logic

---
+++ app/javascript/controllers/components/Greeting.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getGreetings } from "../../redux/slices/greetingSlice";
+
+const Greeting = (props) => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [])
+
+  const greetings = useSelector((state) => state.greetings.greetings);
+  const { greeting } = greetings;
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  )
+};
+
+export default Greeting;
