728f5d0b461e3c448c4c13e2c7d8e1216c5fbd92
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-28 21:54:21 +0200

    Create seed data and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting: "Hallo World")
+Greeting.create(greeting: "Good Morning")
