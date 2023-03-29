64248dfb8e6e5484344a58a95fcda176aea20481
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-29 08:02:22 +0200

    Add extra dependecies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
 import React from "react"
 import ReactDOM from "react-dom"
 import { NavLink } from "react-router-dom";
